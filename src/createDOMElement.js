2595c39799b61fc82f5428b647d1823439742283
Aminady <aminasady56@gmail.com>
2022-10-27 13:13:42 +0000

    .

--- src/createDOMElement.js
+++ src/createDOMElement.js
@@ -55,17 +55,17 @@ function createTaskModal(){
       <input type="date" name="date" id="date">
       </div>
       
-      <div>
-      <select name="tasks-or-projects" id="tasks-or-projects">
-          <option value="tasks-or-projects">Task or Project ?</option>
-          <option value="tasks-option">My Tasks</option>
-          <option value="projects-option">My Projects</option>
-      </select>
-      </div>
-      
       <button type="button" class="submit-task">Add to your list</button>
       </form>
       `
+      // <div>
+      // <select name="tasks-or-projects" id="tasks-or-projects">
+      //     <option value="tasks-or-projects">Task or Project ?</option>
+      //     <option value="tasks-option">My Tasks</option>
+      //     <option value="projects-option">My Projects</option>
+      // </select>
+      // </div>
+      
   const modal = document.querySelector('.modal');
 
   modal.addEventListener("click", () => {
@@ -81,19 +81,17 @@ function submitTasksModal() {
   const tasksOrProjects = document.querySelector('#tasks-or-projects');
   const dateInput = document.querySelector('#date');
   const tasksName = document.querySelector('#tasks-name');
-
+  function newModal (name, date, type) {
+      this.name = name;
+      this.date = date;
+      this.type = type
+  };
+  
   submitModal.addEventListener('click', () => {
-      function newObject (name, date, type) {
-          this.name = name;
-          this.date = date;
-          this.type = type
-      };
-
       let tasksArray = [];
       let projectsArray = [];
 
-      if(tasksOrProjects.value === "tasks-option") {
-          tasksArray.push(new newObject(tasksName.value, dateInput.value, tasksOrProjects.value))
+          tasksArray.push(new newModal(tasksName.value, dateInput.value))
           modal.parentNode.remove();
 
           return tasksProjectsContainer.innerHTML += 
@@ -105,7 +103,6 @@ function submitTasksModal() {
           <svg class="trash-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
           </div>
           `
-      }    
       modal.parentNode.remove();
       
   });
