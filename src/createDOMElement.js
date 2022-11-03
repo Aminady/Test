6ffd57747d1c20255d180b56ca8f81055c23a77a
Aminady <aminasady56@gmail.com>
2022-11-03 15:02:25 +0000

    .

--- src/createDOMElement.js
+++ src/createDOMElement.js
@@ -1,7 +1,14 @@
 import { mainContainer, tasksProjectsContainer, dateParser} from ".";
 
 let date = new Date();
-var modal = document.querySelector('.modal');
+var modalContainer = document.querySelector('.project-modal-container');
+
+function removeModal() { 
+  var modal = document.querySelector('.modal');
+  modal.addEventListener("click", () => {
+      modal.parentNode.remove();
+  });
+      }
 
 function injectTasks () {
     return tasksProjectsContainer.innerHTML = `
@@ -66,13 +73,7 @@ function createTaskModal(){
       //     <option value="projects-option">My Projects</option>
       // </select>
       // </div>
-      
-  var modal = document.querySelector('.modal');
-
-  modal.addEventListener("click", () => {
-      modal.parentNode.remove();
-  });
-
+  // removeModal();
 }
 
 function createProjectModal() {
@@ -98,11 +99,6 @@ function createProjectModal() {
         <button type="button" class="submit-project">Add this project</button>
         </form>
         `
-        const modal = document.querySelector('.modal');
-
-        modal.addEventListener("click", () => {
-            modal.parentNode.remove();
-        });
   
 }
 
@@ -142,7 +138,10 @@ function submitProjectModal() {
 
   
       let projectsArray = [];
+      
       projectsArray.push(new newModal(titleName.value, taskName.value, dateInput.value));
+      // modalContainer.remove()
+      console.log(modalContainer);
       emptyProjects.remove();
 
       tasksProjectsContainer.innerHTML += 
@@ -161,11 +160,6 @@ function submitProjectModal() {
       <svg class="trash-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
       </div>
   `
-  // modal.parentNode.remove();
-
-//   modal.addEventListener("click", () => {
-//     modal.parentNode.remove();
-// });
 };
 
   
